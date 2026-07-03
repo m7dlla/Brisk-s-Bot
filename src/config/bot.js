549f8a061ead15c364f0509f746c5b97fc4b9db6
m7dlla <m7dlla@gmail.com>
2026-07-03 16:31:30 +0400

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Bot made by Brisk.",
+        name: "Isn't it working?",
         // Activity type number (0 = Playing).
         type: 0,
       },
